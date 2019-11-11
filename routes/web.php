6a2317ec05c4c78596dba4c07c7a022a6c0eeb3a
Amir Daneshkar <amir.daneshkar@hotmail.com>
2019-11-11 16:26:06 +0330

    A user may publish threads

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/threads', 'ThreadController@index')->name('thread.index');
-Route::get('/threads/{thread}', 'ThreadController@show')->name('thread.show');
-Route::post('/threads/{thread}/replies', 'ReplyController@store')->name('reply.store');
+Route::get('/threads', 'ThreadController@index')->name('threads.index');
+Route::post('/threads', 'ThreadController@store')->name('threads.store');
+Route::get('/threads/{thread}', 'ThreadController@show')->name('threads.show');
+Route::post('/threads/{thread}/replies', 'ReplyController@store')->name('replies.store');
 
