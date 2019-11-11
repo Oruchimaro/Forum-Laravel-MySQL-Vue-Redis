b77647e5b1a96aa1fb4d7fd199924deb1190c5cd
Amir Daneshkar <amir.daneshkar@hotmail.com>
2019-11-11 12:59:12 +0330

    Threads and Single Thread (index and show method) bulid. path() method added

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,6 @@
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
 |
 */
 
@@ -19,6 +16,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
+Route::get('/threads', 'ThreadController@index')->name('thread.index');
+Route::get('/threads/{thread}', 'ThreadController@show')->name('thread.show');
 
-Route::get('/home', 'HomeController@index')->name('home');
