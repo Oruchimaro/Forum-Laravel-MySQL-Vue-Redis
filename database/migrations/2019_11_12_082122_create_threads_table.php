594f7c9b5fe2c56d1c2925e4dd148a2594a4b64a
Amir Daneshkar <amir.daneshkar@hotmail.com>
2020-01-17 09:12:01 +0330

    Confusing Errors and Solutions
    Set a foreign-key constraint on threads table for best_reply_id
    to be set null after deletion of the reply
    But the errors are freaking annoying.
    Overall, renamed the migration to change the order of them and then
    made the typr of keys match.

--- database/migrations/2019_11_12_082122_create_threads_table.php
+++ database/migrations/2019_11_12_082122_create_threads_table.php
@@ -18,7 +18,15 @@ class CreateThreadsTable extends Migration
             $table->string('slug')->unique()->nullable();
             $table->unsignedInteger('user_id');
             $table->unsignedInteger('channel_id');
-            $table->unsignedInteger('best_reply_id')->nullable();
+            $table->unsignedBigInteger('best_reply_id')->nullable();
+
+            //setting the threads best_reply_id null on reply deletion
+            //but this will make the problem of which table is being created first
+            $table->foreign('best_reply_id')
+                ->references('id')
+                ->on('replies')
+                ->onDelete('set null');
+
             $table->unsignedInteger('replies_count')->default(0);
             $table->unsignedInteger('visits')->default(0);
             $table->string('title');
